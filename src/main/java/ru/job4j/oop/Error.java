34d81aa20e24524faf1523c1b471afd035a41a18
Windows <ivinjc@gmail.com>
2022-05-04 19:02:28 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void print() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, -1, "Error");
+        Error er = new Error(false, 0, "Process finished with exit code 0");
+        Error er2 = new Error();
+        error.print();
+        er.print();
+        er2.print();
+    }
+}
