02adedc760255a64117bc87f01df2bd6e6e5dfe3
Windows <ivinjc@gmail.com>
2022-05-06 16:37:10 +0300

    Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int size = 0;
+        Item[] copyitems = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                copyitems[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(copyitems, size);
+    }
+
+    public Item[] findByName(String key) {
+        int size = 0;
+        Item[] copyitems = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                break;
+            } else if (key.equals(items[i].getName())) {
+                copyitems[i] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(copyitems, size);
     }
 }
