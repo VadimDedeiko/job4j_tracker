fceac373105b6710a743725590792f7f2bf4d6c8
Windows <ivinjc@gmail.com>
2022-05-07 11:25:15 +0300

    toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,6 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(dateTimeFormatter));
+        System.out.println(item);
     }
 }
