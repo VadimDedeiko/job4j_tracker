e8141c3599a4feb2ee971a0d594163421979c96b
Windows <ivinjc@gmail.com>
2022-05-07 15:24:42 +0300

    Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Freshman.java
@@ -0,0 +1,6 @@
+package ru.job4j.oop;
+
+public class Freshman extends Students {
+    Students student = new Freshman();
+    Object object = student;
+}
