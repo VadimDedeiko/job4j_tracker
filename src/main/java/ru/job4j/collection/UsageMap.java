346992da7dac899f524744c5a5130deaa3c79a6e
Windows <ivinjc@gmail.com>
2022-05-13 08:17:03 +0300

    Map, HashMap [#212652 #300140]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,7 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("ivinjc@gmail.com", "Dedeyko Vadim");
-        for (String key: map.keySet()) {
+        for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + "-" + value);
         }
