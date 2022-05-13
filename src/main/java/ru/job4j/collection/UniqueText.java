66c18bbbcf4253ba07faac3d2e1c8f6c9502a97b
Windows <ivinjc@gmail.com>
2022-05-13 21:16:38 +0300

    Revert "Комбинированный компаратор [#10036]"
    This reverts commit edaabf8fbde285648b6cf6237a7d2064d3370fed.

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,17 +4,16 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
-        for (String str : origin) {
+        for (String str: origin) {
             check.add(str);
         }
-        for (String str : text) {
-            if (!check.contains(str)) {
-                return false;
+        for (String str: text) {
+            return check.contains(str);
             }
-        }
-        return true;
+        return rsl;
     }
 }
