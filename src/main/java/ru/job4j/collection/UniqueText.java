6c300a131441ab4cd2411126e1c1fea227338c0d
Windows <ivinjc@gmail.com>
2022-05-14 21:06:41 +0300

    Идентичные тексты [#212655 #300080]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,17 +4,19 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-
         boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
-        for (String str: origin) {
+        for (String str : origin) {
             check.add(str);
         }
-        for (String str: text) {
-            return check.contains(str);
+        for (String str : text) {
+            if (!check.contains(str)) {
+                rsl = false;
+                break;
             }
+        }
         return rsl;
     }
 }
--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -1,11 +1,14 @@
 package ru.job4j.function;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class SearchFolder {
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) -> right.compareTo(left);
         List<Folder> lst = new ArrayList<>();
         for (Folder f : list) {
             if (pred.test(f)) {
