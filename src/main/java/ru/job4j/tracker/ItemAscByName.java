62fcfbf4df3a5edfd0d24168ec07d98f22e0edc4
Windows <ivinjc@gmail.com>
2022-05-14 21:19:44 +0300

    Сортировка [#10096 #300630]

--- src/main/java/ru/job4j/tracker/ItemAscByName.java
+++ src/main/java/ru/job4j/tracker/ItemAscByName.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class ItemAscByName implements Comparator<Item> {
     @Override
     public int compare(Item first, Item second) {
-        return first.getName().compareTo(second.getName());
+        return second.getName().compareTo(first.getName());
     }
 }
