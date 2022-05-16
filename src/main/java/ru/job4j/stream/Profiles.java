0e54a919cb2a2bbfc08ee283f535cef8351ca7db
Lenovo 320 <ivinjc@gmail.com>
2022-05-16 09:38:46 +0300

    Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,6 +1,8 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +13,15 @@ public class Profiles {
         adress = profiles.stream()
                 .map(s -> s.getAddress())
                 .collect(Collectors.toList());
+        return adress;
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        List<Address> adress = new ArrayList<>();
+        adress = collect(profiles)
+                .stream()
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct().toList();
         return adress;
     }
 }
